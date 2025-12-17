3a2cfc4e5cbbb6232a934fc44db56a3c7cac2dba
DamiancioSkylines <damiancioskylines@gmail.com>
2025-12-17 14:32:02 +0000

    - Publish Configuration files

--- Publish Mod Command.bat
+++
@@ -1,4 +0,0 @@
-"G:\SteamLibrary\steamapps\common\Cities Skylines II\Cities2_Data\Content\Game\.ModdingToolchain\ModPublisher\ModPublisher.exe" "Publish" "C:\Users\damia\RiderProjects\CandyCommercialLUT\Properties\PublishConfiguration.xml" -l "G:\Game things\Cities Skylines 2\My Mods\pdx_account\pdx_account.txt" -c "C:\Users\damia\RiderProjects\CandyCommercialLUT\bin\Release\net48" -v
-pause
-
-
